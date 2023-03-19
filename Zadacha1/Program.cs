d20bb82d61f5fe58af57e1747e910b057571eb9b
ROMAN <junk0410448@mail.ru>
2023-03-19 18:44:47 +0700

    Zadacha 2

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -1,11 +1,16 @@
-﻿int Promp (string message)
+﻿//  Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+int Prompt (string message)
 {
     System.Console.Write(message);
     int result = Convert.ToInt32(Console.ReadLine());
     return result;
 }
-int number = Promp("Введите трехзначное число: ");
-if (number < 100 || number>=1000)
+int number = Prompt("Введите трехзначное число: ");
+if (number < 100 || number >= 1000)
 {
     Console.WriteLine("Вы ввели не трехзначное число, попробуйте еще раз");
     return;
---
+++ Zadacha2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру числа или сообщает,
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int number = Prompt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write($"В числе {number} третья цифра {MakeArray(number, count)}");
+int Prompt (string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int MakeArray(int a, int b)
+{
+int result = -1;
+    if (b < 3)
+    {
+        Console.Write("Вы ввели не трехзначное число, попробуйте еще раз  ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
