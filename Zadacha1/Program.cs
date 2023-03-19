cdc66b9393c6164b459c995b73ae71970502fb10
ROMAN <junk0410448@mail.ru>
2023-03-19 16:07:12 +0700

    Zadacha 1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,17 @@
+﻿int Promp (string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int number = Promp("Введите трехзначное число: ");
+if (number < 100 || number>=1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, попробуйте еще раз");
+    return;
+}
+else
+{
+    int secondValue = ((number / 10) % 10);
+    Console.WriteLine($"В числе {number} вторая цифра {secondValue}");
+}
